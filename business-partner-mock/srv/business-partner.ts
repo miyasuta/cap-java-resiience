d0a5b305f2ea4fb4b8dec272f39eeb9afcbe3265
Mio Yasutake <mio.fujita.01@gmail.com>
2025-09-29 05:11:06 +0900

    update dependencies to version 4.x and comment out READ event handler in BusinessPartnerService

--- business-partner-mock/srv/business-partner.ts
+++ business-partner-mock/srv/business-partner.ts
@@ -4,9 +4,9 @@ import { BusinessPartners } from '#cds-models/BusinessPartnerService'
 export class BusinessPartnerService extends cds.ApplicationService { async init() {
   const bupa = await cds.connect.to('BusinessPartnerA2X')
 
-  this.on ('READ', BusinessPartners, async (req) => {
-    return bupa.run(req.query)
-  })
+  // this.on ('READ', BusinessPartners, async (req) => {
+  //   return bupa.run(req.query)
+  // })
 
   return super.init()
 }}
